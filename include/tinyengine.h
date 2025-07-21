e4b4d42a3e3b4fba2c407030d4793677fd500315
NomadicSasquatch <boyu.chong@gmail.com>
2025-07-21 17:38:33 +0800

    Chore: Testing Ubuntu and WSL setup. Imports for tinyengine.

---
+++ include/tinyengine.h
@@ -0,0 +1,34 @@
+#ifndef TINYENGINE_H
+#define TINYENGINE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdatomic.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+#include <inttypes.h>
+#include <time.h>
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
