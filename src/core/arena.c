cf8b24373d8064a2bd3723f98589cbf2032d53f7
Nomadic Sasquatch <boyu.chong@gmail.com>
2026-03-15 21:22:36 +0800

    Add smoketest for arena

--- src/core/arena.c
+++ src/core/arena.c
@@ -1,5 +1,24 @@
-#include "tinyengine.h"
+#ifdef ARENA_SELFTEST_MAIN
+#include <stdio.h>
 #include "arena.h"
 
-#include <stdalign.h>
+int main(void) {
+    Arena a;
+    arena_init(&a, 4096);
 
+    int *p = arena_alloc(&a, sizeof(int), _Alignof(int));
+    *p = 123;
+    printf("arena selftest ok: %d\n", *p);
+
+    int *p2 = arena_alloc(&a, sizeof(int), _Alignof(int));
+    *p2 = 456;
+    printf("arena selftest ok: %d\n", *p2);
+
+    arena_reset(&a);
+    printf("Has been reset: %d\n", (int)(((void*)a.curr) == ((void*)p)));
+
+    arena_free(&a);
+    return 0;
+}
+
+#endif
